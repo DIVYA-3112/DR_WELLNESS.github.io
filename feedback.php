90f05f03836e358206d742d9e7cb4322d6a9cd62
DIVYA-3112 <divyathakkar3112@gmail.com>
2023-05-15 17:24:04 +0530

    Update feedback.php

--- feedback.php
+++ feedback.php
@@ -1,40 +1,81 @@
 <?php
-session_start();
 
 $servername = "localhost";
-$username = "root";
-$password = "1234";
-$dbname = "details";
- 
+$username = "divya3112";
+$password = "divya1234";
+
 // Create connection
-$conn = new mysqli($servername,$username, $password, $dbname);
- 
-// Check connection
+$conn = new mysqli($servername,$username, $password);
+
+// check connection
 if ($conn->connect_error) {
-    die("Connection failed: "
-        . $conn->connect_error);
+    die("Connection failed: " . $conn->connect_error);
 }
 
-$sqlquery="CREATE TABLE IF NOT EXISTS feedback (NAME VARCHAR(100),THERAPY VARCHAR(100),THERAPY_RATING INT,INTERFACE INT,OVERALL INT,COMMENT VARCHAR(1024));";
-$conn->query($sqlquery);
+else {
+    echo "<br><br>Connection successful<br><br>";
+}
 
+// test input function
+function test_input($data) {
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+}
 
+// create database
+$sql = "CREATE DATABASE FEEDBACK";
 
-$uname=$_POST["username"];
-$therapy=$_POST["therapy"];
-$trate=$_POST["therapy-rating"];
-$inter=$_POST["interface"];
-$over=$_POST["overall"];
-$comment=$_POST["comments"];
-
-
-$sqlquery = "INSERT INTO feedback VALUES('$uname','$therapy','$trate','$inter','$over','$comment');";
-    
-if($conn->query($sqlquery) === TRUE) {
-    header("Location: html code.html");
-    exit();
+if ($conn->query($sql) === TRUE) {
+  echo "<br><br>Database created successfully<br><br>";
 } else {
-    echo "Error: " . $sqlquery . "<br>" . $conn->error;
+  echo "<br><br>Error creating database: <br><br>" . $conn->error;
 }
-session_destroy();
+
+
+// create variables for input.
+
+$username = $therapy = $therapyrating = $interfacerating = $overallrating = $comments = "";
+
+$username = ($_POST['username']);
+$therapy = ($_POST['therapy']);
+$therapyrating = ($_POST['therapy-rating']);
+$interfacerating = ($_POST['interface']);
+$overallrating = ($_POST['overall']);
+$comments = ($_POST['comments']);
+
+
+//create table
+$sql = "CREATE TABLE FEEDBACK.FBTABLE (
+    username VARCHAR(30),
+    therapy VARCHAR(30),
+    therapyrating INT,
+    interfacerating INT,
+    overallrating INT,
+    comments VARCHAR(30)
+    )";
+
+// check table creation
+if ($conn->query($sql) === TRUE) {
+    echo "<br><br>Table 'fbtable' created successfully<br><br>";
+} else {
+    echo "<br><br>table: <br><br>" . $conn->error;
+}
+
+// insert data 
+$sql = "INSERT INTO feedback.fbtable (username, therapy, comments)
+VALUES ('$username', '$therapy', '$comments')";
+
+// check if inserted or not
+if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+    } else {
+    echo "<br><br>Error: <br><br><br>" . $conn->error;
+    }
+
+
+// close connection 
+$conn->close();
+
 ?>
